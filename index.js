b33c0acaebb7c9f2e2b9279974193b03770cedf5
jackie <jackie@airbears2-10-142-47-11.airbears2.1918.berkeley.edu>
2018-04-20 02:56:48 -0700

    Initial commit

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ var right_arrow = $('.easytransitions_navigation__right'); // Element that trigg
 var left_arrow = $('.easytransitions_navigation__left'); // Element that trigger move left
 var slide_amount = $('.easytransitions section').length; // How many slides
 var current_slide = 1; // Starting slide
-var about = $('.about');
+var about = $('.about'); // about navigation
 var projects = $('.projects'); // projects navigation
+var photography = $('.photography'); // photography navigation
 
 right_arrow.click(function(){
   if(current_slide < slide_amount){
@@ -62,6 +63,20 @@ projects.on('click', function(){
   }
 });
 
+// PHOTOGRAPHY NAVIGATION
+photography.on('click', function(){
+  if(current_slide !== 3){
+    current_slide = 3;
+    close_modals();
+    var active_slide = $('#photography_slide')
+    set_transition(active_slide);
+    setTimeout(function(){
+      $('.active_slide').hide().removeClass('active_slide');
+      $('#photography_slide').addClass('active_slide').show();
+    },change_point);
+  }
+});
+
 // Set transition type
 
 function set_transition(tran){
@@ -81,16 +96,25 @@ function set_transition(tran){
 var modal_1_content = document.getElementById("modal_1_content");
 var modal_2_content = document.getElementById("modal_2_content");
 var modal_3_content = document.getElementById("modal_3_content");
+var modal_4_content = document.getElementById("modal_4_content");
+var modal_5_content = document.getElementById("modal_5_content");
+var modal_6_content = document.getElementById("modal_6_content");
 
 // Get the button that opens the modal
 var modal_1 = document.getElementById("modal_1");
 var modal_2 = document.getElementById("modal_2");
 var modal_3 = document.getElementById("modal_3");
+var modal_4 = document.getElementById("modal_4");
+var modal_5 = document.getElementById("modal_5");
+var modal_6 = document.getElementById("modal_6");
 
 // Get the <span> element that closes the modal
 var span_1 = document.getElementsByClassName("close")[0];
 var span_2 = document.getElementsByClassName("close")[1];
 var span_3 = document.getElementsByClassName("close")[2];
+var span_4 = document.getElementsByClassName("close")[3];
+var span_5 = document.getElementsByClassName("close")[4];
+var span_6 = document.getElementsByClassName("close")[5];
 
 // When the user clicks on the button, open the modal
 modal_1.onclick = function() {
@@ -102,6 +126,15 @@ modal_2.onclick = function() {
 modal_3.onclick = function() {
     modal_3_content.style.display = "block";
 }
+modal_4.onclick = function() {
+    modal_4_content.style.display = "block";
+}
+modal_5.onclick = function() {
+    modal_5_content.style.display = "block";
+}
+modal_6.onclick = function() {
+    modal_6_content.style.display = "block";
+}
 // When the user clicks on <span> (x), close the modal
 span_1.onclick = function() {
     modal_1_content.style.display = "none";
@@ -111,6 +144,15 @@ span_2.onclick = function() {
 }
 span_3.onclick = function() {
     modal_3_content.style.display = "none";
+}
+span_4.onclick = function() {
+    modal_4_content.style.display = "none";
+}
+span_5.onclick = function() {
+    modal_5_content.style.display = "none";
+}
+span_6.onclick = function() {
+    modal_6_content.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
@@ -124,6 +166,15 @@ window.onclick = function(event) {
     if (event.target == modal_3_content) {
         modal_3_content.style.display = "none";
     }
+    if (event.target == modal_4_content) {
+        modal_4_content.style.display = "none";
+    }
+    if (event.target == modal_5_content) {
+        modal_5_content.style.display = "none";
+    }
+    if (event.target == modal_6_content) {
+        modal_6_content.style.display = "none";
+    }
 }
 
 // Close all when changing pages
@@ -131,4 +182,7 @@ function close_modals() {
   modal_1_content.style.display = "none";
   modal_2_content.style.display = "none";
   modal_3_content.style.display = "none";
+  modal_4_content.style.display = "none";
+  modal_5_content.style.display = "none";
+  modal_6_content.style.display = "none";
 }
