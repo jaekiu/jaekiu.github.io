a7d90a8f447b520ec06791b491be0ee38da6bbc3
Jacqueline Zhang <jackiegzhang@gmail.com>
2020-08-06 23:30:47 -0700

    hamburger on da menu

--- script.js
+++ script.js
@@ -18,3 +18,19 @@ function leaveButton() { animateButton('90%') };
 
 button.addEventListener('mouseenter', enterButton, false);
 button.addEventListener('mouseleave', leaveButton, false);
+
+const menuBtn = document.querySelector('.menu');
+const navUl = document.querySelector('.nav-ul');
+
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+    if (!menuOpen) {
+        menuBtn.classList.add('open');
+        navUl.classList.add('show');
+        menuOpen = true;
+    } else {
+        menuBtn.classList.remove('open');
+        navUl.classList.remove('show');
+        menuOpen = false;
+    }
+})
